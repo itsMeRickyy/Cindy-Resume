dc6afee8f232b9db387f99b032ef43780aae901c
Ryoiki15 <hiirizkii@gmail.com>
2023-02-26 00:34:24 +0700

    Membuat bagian header dan button

--- public/script/event.js
+++ public/script/event.js
@@ -1,4 +1,5 @@
 const toggle = document.getElementById("toggle");
+const menu = document.getElementById("mobileMenu");
 
 toggle.addEventListener("click", () => {
   const menu = document.getElementById("mobileMenu");
@@ -6,5 +7,15 @@ toggle.addEventListener("click", () => {
     menu.style.display = "none";
   } else {
     menu.style.display = "block";
+  }
+});
+
+// toggle.addEventListener("click", () => {
+//   document.getElementById("mobileMenu").classList.toggle("hidden");
+// });
+
+document.addEventListener("click", (e) => {
+  if (!toggle.contains(e.target) && !menu.contains(e.target)) {
+    menu.style.display = "none";
   }
 });
