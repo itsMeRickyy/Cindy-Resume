532da2265053c2b069b5a37e4ba168805588297f
Ryoiki15 <hiirizkii@gmail.com>
2023-03-04 16:33:40 +0700

    membuat bagian what i do dan bagian portfolio

--- public/script/event.js
+++ public/script/event.js
@@ -1,6 +1,6 @@
 const toggle = document.getElementById("toggle");
 const menu = document.getElementById("mobileMenu");
-const listMenu = menu.querySelector("ul li a");
+const listMenu = menu.querySelector("#menu .mobileBtn button");
 
 toggle.addEventListener("click", () => {
   const menu = document.getElementById("mobileMenu");
@@ -21,8 +21,54 @@ document.addEventListener("click", (e) => {
   }
 });
 
-listMenu.addEventListener("click", (e) => {
-  if (listMenu.contains(e.target)) {
-    menu.style.display = "none";
+// listMenu.addEventListener("click", (e) => {
+//   if (listMenu.contains(e.target)) {
+//     menu.style.display = "none";
+//   }
+// });
+
+// What i do button
+
+const btnWhatIdo = document.getElementById("show-what-i-do-btn");
+const menuWhatIdo = document.getElementById("What_i_Do");
+
+btnWhatIdo.addEventListener("click", () => {
+  if (menuWhatIdo.style.display === "flex") {
+    menuWhatIdo.style.display = "none";
+  } else {
+    menuWhatIdo.style.display = "flex";
   }
+});
+
+// Button project
+const projectBtn = document.getElementById("projectBtn");
+const projectList = document.querySelectorAll(".projectList");
+const closeProjectBtn = document.getElementById("closeProjectBtn");
+// const project2 = document.getElementById("project2");
+// const project3 = document.getElementById("project3");
+
+projectBtn.addEventListener("click", () => {
+  for (let i = 0; i < projectList.length; i++) {
+    if (projectList[i].style.display === "flex") {
+      projectList[i].style.display = "none";
+    } else {
+      projectList[i].style.display = "flex";
+    }
+  }
+  projectBtn.style.display = "none";
+  closeProjectBtn.style.display = "flex";
+  console.log("oke!");
+});
+
+closeProjectBtn.addEventListener("click", () => {
+  for (let i = 0; i < projectList.length; i++) {
+    if (projectList[i].style.display === "flex") {
+      projectList[i].style.display = "none";
+    } else {
+      projectList[i].style.display = "flex";
+    }
+  }
+
+  closeProjectBtn.style.display = "none";
+  console.log("oke!");
 });
